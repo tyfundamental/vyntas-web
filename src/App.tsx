13174669ef460867b5f7e2fb2e2e54328b0b1999
DANI <info@vyntas.com>
2026-06-11 19:27:52 +0200

    App simple

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,10 @@
 import { LanguageProvider } from './context/LanguageContext';
 import { Header } from './components/Header';
-import { Hero } from './components/Hero';
-import { AboutSection } from './components/AboutSection';
-import { ScienceSection } from './components/ScienceSection';
-import { ProtocolSection } from './components/ProtocolSection';
-import { SettleNewsletterBanner } from './components/SettleNewsletterBanner';
-import { Footer } from './components/Footer';
-
-function AppContent() {
-  return (
-    <div className="min-h-screen bg-white">
-      <Header />
-      <Hero />
-      <AboutSection />
-      <ScienceSection />
-      <ProtocolSection />
-      <SettleNewsletterBanner />
-      <Footer />
-    </div>
-  );
-}
 
 export default function App() {
   return (
     <LanguageProvider>
-      <AppContent />
+      <Header />
     </LanguageProvider>
   );
 }
